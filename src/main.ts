7e5c7cad70433995e360bd1fccc7fcde5be2c0f6
John Vacacela <johnl7u7@gmail.com>
2025-07-01 23:07:35 -0500

    feat: update CORS configuration to support multiple origins

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://q8bkbqgl-5173.use2.devtunnels.ms',
+    ],
     credentials: true,
-  })
+  });
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
