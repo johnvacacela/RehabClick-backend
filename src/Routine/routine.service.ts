170a3f96b7825d8e09caab65e9c5c7acee69b5fd
OssianGH <poroscarin@gmail.com>
2025-07-15 18:30:32 -0500

    feat: add method to retrieve routines by therapist ID in RoutineService

--- src/Routine/routine.service.ts
+++ src/Routine/routine.service.ts
@@ -15,6 +15,50 @@ export class RoutineService {
     });
   }
 
+  async getAllRoutinesByTherapistId(id_terapeuta: number) {
+    const routines = await this.prisma.rutina.findMany({
+      where: {
+        terapeuta_paciente: {
+          terapeuta: {
+            id: id_terapeuta,
+          },
+        },
+      },
+      select: {
+        id: true,
+        id_terapeuta_paciente: true,
+        nombre: true,
+        ejercicio_rutina: {
+          select: {
+            id: true,
+            repeticiones: true,
+            ejercicio: {
+              select: {
+                nombre: true,
+                video_url: true,
+              },
+            },
+          },
+          orderBy: {
+            orden: 'asc',
+          },
+        },
+      },
+    });
+
+    return routines.map((routine) => ({
+      id: routine.id,
+      id_terapeuta_paciente: routine.id_terapeuta_paciente,
+      nombre: routine.nombre,
+      ejercicios: routine.ejercicio_rutina.map((er) => ({
+        id: er.id,
+        nombre: er.ejercicio.nombre,
+        repeticiones: er.repeticiones,
+        video_url: er.ejercicio.video_url,
+      })),
+    }));
+  }
+
   async getAllRoutinesByIdTherapistPatient(id_terapeuta_paciente: number) {
     return await this.prisma.rutina.findMany({
       where: {
@@ -24,7 +68,7 @@ export class RoutineService {
         ejercicio_rutina: {
           include: {
             ejercicio: true, // Incluye los ejercicios de la rutina
-          }
+          },
         },
         terapeuta_paciente: {
           include: {
